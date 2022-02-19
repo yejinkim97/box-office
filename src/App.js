5895efae77026ca567699f1218cbc02767828b96
Yejin <yejinkim5979@gmail.com>
2022-02-18 22:48:22 -0500

    created nav bar and pages components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from "react";
-function App() {
-  return <div>hi</div>;
-}
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Navs from './Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
+const App = () => {
+  return (
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route>
+          <div>Not Found</div>
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
