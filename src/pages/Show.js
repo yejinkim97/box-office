9bf427e44b0a94007813900ce2d32ff718564165
Yejin <yejinkim5979@gmail.com>
2022-02-19 11:44:59 -0500

    added loading and error to show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,14 +5,40 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log(show);
+
+  if (isLoading) {
+    return <div>Being Loading</div>;
+  }
+
+  if (error) {
+    return <div>Error occured</div>;
+  }
 
   return <div>this is show page</div>;
 };
