c6a3060e4b5e5a476853a60c4fa89a34c24c5dd8
Yejin <yejinkim5979@gmail.com>
2022-02-18 23:38:25 -0500

    added layout and title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './Navs';
+
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 const App = () => {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route>
-          <div>Not Found</div>
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route>
+        <div>Not Found</div>
+      </Route>
+    </Switch>
   );
 };
 
--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 const Starred = () => {
-  return <div>This is starred page</div>;
+  return <MainPageLayout>This is starred page</MainPageLayout>;
 };
 
 export default Starred;
